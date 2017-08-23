332a3363a33da57d7f5892e19a84d0035de04a12
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2017-08-23 16:33:35 +0200

    XBTUtility.Division

--- source/XBTUtility.cpp
+++ source/XBTUtility.cpp
@@ -1,5 +1,31 @@
 #include <stdio.h>
+#include <assert.h>
 
+// Prototypes:
+void XBTPrivateTrace (const char  * theMessage,  const char * theFileName, const int theLine, const char * theFunction);
+int Division (const int theNumerator, const int theDenominator);
+bool DivisionIf (const int theNumerator, const int theDenominator, int * theResult);
+
+
+// Definitions:
 void XBTPrivateTrace (const char  * theMessage,  const char * theFileName, const int theLine, const char * theFunction){
+	assert (NULL != theMessage);
+	assert (NULL != theFileName);
+	assert (NULL != theFunction);
 	printf ("Message: %s, File name: %s; Line: %d, func: %s;\n", theMessage, theFileName, theLine, theFunction);
+}
+
+int Division (const int theNumerator, const int theDenominator){
+	assert (0 != theDenominator);
+	return theNumerator/theDenominator;
+}
+
+bool DivisionIf (const int theNumerator, const int theDenominator, int * theResult){
+	assert (NULL != theResult);
+	bool ret = false;
+	if (0 != theDenominator){
+		ret = true;
+		*theResult = Division (theNumerator, theDenominator);
+	}
+	return ret;
 }
--- source/XBTUtility.h
+++ source/XBTUtility.h
@@ -8,6 +8,7 @@
 #define XBTTrace(theMessage) XBTPrivateTrace ((theMessage), __FILE__, __LINE__, __func__)
 
 extern void XBTPrivateTrace (const char  * theMessage,  const char * theFileName, const int theLine, const char * theFunction);
-
+extern int Division (const int theNumerator, const int theDenominator);
+extern bool DivisionIf (const int theNumerator, const int theDenominator, int * theResult);
 
 #endif
