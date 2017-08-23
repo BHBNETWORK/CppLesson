04b5b3924c8946f857739ac91a82d7197056ba94
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2017-08-23 12:54:18 +0200

    pseudo macro (__FILE__ etc..)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main (){
-	printf ("Hello World\n");
+	//  char, int, float, double.
+	// signed or unsigned
+	// short 4 bytes or long 8 bytes
+	//	const char * aString = "Hello World\n";
+	const char aString [] = {'H', 'e', 'l', 'l', 'o', 0};
+	printf ("%p", aString);
 	return 0;
 }
 
