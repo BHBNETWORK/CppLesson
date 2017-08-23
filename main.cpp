0e0d9c0d34094563cbd1aec090843585a8fd1e35
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2017-08-23 14:54:18 +0200

    XBTTrace

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
+#ifdef XBTTrace
+#error "XBTrace already defined"
+#endif
+
+#define XBTTrace(theMessage) XBTPrivateTrace ((theMessage), __FILE__, __LINE__, __func__)
+
+static void XBTPrivateTrace (const char  * theMessage,  const char * theFileName, const int theLine, const char * theFunction){
+	printf ("Message: %s, File name: %s; Line: %d, func: %s;\n", theMessage, theFileName, theLine, theFunction);
+}
+
 int main (){
 	//  char, int, float, double.
 	// signed or unsigned
 	// short 4 bytes or long 8 bytes
 	//	const char * aString = "Hello World\n";
-	const char aString [] = {'H', 'e', 'l', 'l', 'o', 0};
-	printf ("%p", aString);
+	const char  aString [] ={'H', 'e', 'l', 'l', 'o', 0};
+
+	XBTTrace (aString);
 	return 0;
 }
 
